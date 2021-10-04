98cdf8aeed2197e440b6c44502b2e0180dfc9c6b
Matheus Araujo <moohbr@gmail.com>
2021-10-04 20:39:01 -0300

    implemented function "uniao"

--- main.c
+++ main.c
@@ -7,9 +7,9 @@ int iniciaVetor(int size){
   return &vector;
 }
 
-int sort(*vector, sizeV){ // Algoritmo de ordenação
+void sort(*vector, sizeV){ // Algoritmo de ordenação
   int j, last_element = 0;
-  for(int i = 0, i = sizeV, i++){
+  for(int i = 0, i < sizeV, i++){
     last_element = vector[i];
     j = i - 1;
     while((j >= 0) && (vector[j] > last_element)){
@@ -20,7 +20,7 @@ int sort(*vector, sizeV){ // Algoritmo de ordenação
   }
 }
 
-int interseccao(vectorA, sizeA, vectorB, sizeB, * sizeC){
+int interseccao(vectorA, sizeA, vectorB, sizeB, *sizeC){
   int z = 0;
   vectorAux = iniciaVetor(*sizeC);
   for(int i = 0; i < sizeA, i++){
@@ -37,8 +37,39 @@ int interseccao(vectorA, sizeA, vectorB, sizeB, * sizeC){
   return vectorAux;
 }
 
-int uniao(vectorA, sizeA, vectorB, sizeB, * sizeC){
+void remove_duplicated_elements(*vectorA, *sizeA){
+  for(i = 0; i < sizeA; i++){
+    for(j = i + 1; j < sizeA; ){
+      if( vetorA[j] == vetorA[i] ){
+        for( k = j; k < sizeA; k++ ){
+          vetorA[k] = vetorA[k + 1]
+          sizeA--;
+        }
+      else{
+        j++;
+      }
+      }
+    }
+  }
+}
 
+int uniao(vectorA, sizeA, vectorB, sizeB, *sizeC){
+  vectorA = sort(vectorA, sizeA);
+  vectorB = sort(vectorB, sizeB);
+  remove_duplicated_elements(&vectorA, &sizeA);
+  remove_duplicated_elements(&vectorB, &sizeB);
+  int sizeAux = sizeA + sizeB;
+  vectorAux =  iniciaVetor(sizeAux);
+  int i = 0;
+  for(i = 0, i < sizeA, i++){
+    vectorAux[i] = vectorA[i];
+  }
+  for(k = 0, k < sizeB, k++){
+    vectorAux[i + 1] = vectorB[k];
+    i++;
+  }
+  sort(vectorAux, sizeC);
+  return vectorAux;
 }
 
 //NAO ALTERE A MAIN
